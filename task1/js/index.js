65205af0637293aa878aeede97957c26d0020547
Алиса Герасиенко <avalentine88@mail.ru>
2022-03-26 19:35:19 +0300

    download

---
+++ task1/js/index.js
@@ -0,0 +1,37 @@
+"use strict";
+
+let sum = 85000;
+let sum2 = sum * 90 / 100;
+
+let result = sum > 100000 ? "Сумма к оплате: " + sum2 : "Сумма к оплате: " + sum;
+
+console.log(result);
+
+
+
+let number = 98;
+
+let secondDigit = number % 10;
+let firstDigit = number / 10;
+let sumNumbers =  Math.floor(firstDigit + secondDigit);
+
+
+console.log(sumNumbers);
+
+
+let a = 30;
+let b = 40;
+
+ a = a + b; 
+ b = a - b; 
+ a = a - b; 
+
+console.log(a, b);
+
+
+
+
+
+
+
+
