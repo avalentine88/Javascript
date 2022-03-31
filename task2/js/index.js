3ec8b9197ba03622221399a97770c8b4bbe5fe16
Алиса Герасиенко <avalentine88@mail.ru>
2022-03-31 13:47:43 +0300

    download

--- task2/js/index.js
+++ task2/js/index.js
@@ -22,6 +22,8 @@ switch (code) {
 }
 
 
+
+
 let count = parseInt(prompt("Введите количество правильных ответов:"));
  //  или let count = 56;
 
@@ -37,6 +39,8 @@ if (count >= 90 && count <=100) {
 	console.log("Введите корректное число.");
 } 
 
+	
+	
 	let num = Math.floor(Math.random() * 10);
 	let userNum = parseInt(prompt("Введите положительное число или 0 для выхода"));
 	while (userNum > num || userNum < num) {
@@ -49,6 +53,18 @@ if (count >= 90 && count <=100) {
 		break;
 	if (userNum === 0) console.log("Выход из программы");	
 		break;
+		
 
+}
+
+
+
+let plates = parseInt(prompt("Введите количество немытых тарелок: "));
+let fairy = parseInt(prompt("Введите количество моющего средства: "));
+
+while (plates > 0 && fairy > 0) {
+	plates--; fairy-0.5;
+	console.log(`Остаток моющего средства: ${fairy}`);
+	if (plates === 0 || fairy === 0) console.log(`Остаток тарелок: ${plates}, остаток моющего средства: ${fairy}`);
 }
 
