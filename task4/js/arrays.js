7e1f973bbe36c0bf1d5905f05dc19f925547934d
Алиса Герасиенко <avalentine88@mail.ru>
2022-04-07 11:09:24 +0300

    download

--- task4/js/arrays.js
+++ task4/js/arrays.js
@@ -10,3 +10,21 @@ for(let nums of numbers) {
 }
 console.log(negativeNums);
 
+
+let randomNums = [];
+let i;
+
+
+
+for (i = 1; i <= 7; i++) {
+	function getRandomInRange(min, max) {
+  	return Math.round(Math.random() * (max - min)) + min;
+}
+	randomNums.push(getRandomInRange(5, 300));
+	
+}
+console.log(randomNums);
+
+// пока до конца не доделала задачу
+
+
