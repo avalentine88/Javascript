bf3b9e0c91c35587bda1337b214b1d4b76c47a94
Алиса Герасиенко <avalentine88@mail.ru>
2022-04-18 12:01:38 +0300

    download

--- task7/js/objects.js
+++ task7/js/objects.js
@@ -23,5 +23,51 @@ console.log(booksSort);
 
 
 
+let items = {
+     piano: {
+         title: "Пианино",
+         price: 3000,
+         count: 25
+     },
+     guitar: {
+         title: "Гитара",
+         price: 1200,
+         count: 40
+     },
+     drum: {
+         title: "Барабаны",
+         price: 2700,
+         count: 12
+     },
+     flute: {
+         title: "Флейта",
+         price: 900,
+         count: 50
+     },
+     harp: {
+         title: "Арфа",
+         price: 3400,
+         count: 5
+     }
+ };
+
+
+let itemTitle = prompt("Введите товар");
+let countToCart = +prompt("Введите количество");
+
+function input(items, itemTitle, countToCart) {
+        for (let item in items) {
+            if (itemTitle === items[item].title && countToCart <= items[item].count) {
+                items[item].count = items[item].count - countToCart;
+                console.log("Остаток товара: " + items[item].count);  
+                return true;
+            }        
+        }
+        
+        console.log("Выбранного товара недостаточно в наличии");  
+        return false;           
+}
+   
+console.log(input(items, itemTitle, countToCart));
 
 
